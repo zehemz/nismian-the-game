6122f06001c9414d13b093e42461948ab08dbcc5
Lucas Emmanuel Bais <lucas.bais@mercadolibre.com>
2018-12-29 20:50:58 -0300

    added java8 support

--- core/src/com/bahoga/nismian/systems/EngineSystem.java
+++ core/src/com/bahoga/nismian/systems/EngineSystem.java
@@ -3,13 +3,22 @@ package com.bahoga.nismian.systems;
 import com.badlogic.ashley.core.*;
 import com.badlogic.ashley.utils.ImmutableArray;
 
+import java.util.function.Consumer;
+
 public class EngineSystem extends EntitySystem {
 
-    protected Engine engine;
+    private Engine engine;
 
-    protected ImmutableArray<Entity> getAll(Class<? extends Component>... components) {
+    ImmutableArray<Entity> getAll(final Class<? extends Component>... components) {
         Family all = Family.all(components).get();
         return engine.getEntitiesFor(all);
+    }
+
+    void doApply(final Consumer<Entity> function, final Class<? extends Component>... components) {
+        ImmutableArray<Entity> all = getAll(components);
+        for (Entity en : all) {
+            function.accept(en);
+        }
     }
 
     @Override
--- core/src/com/bahoga/nismian/systems/MapRenderSystem.java
+++ core/src/com/bahoga/nismian/systems/MapRenderSystem.java
@@ -1,24 +1,19 @@
 package com.bahoga.nismian.systems;
 
-import com.badlogic.ashley.core.Entity;
-import com.badlogic.ashley.utils.ImmutableArray;
 import com.bahoga.nismian.Mappers;
 import com.bahoga.nismian.components.CameraComponent;
 import com.bahoga.nismian.components.MapComponent;
+
 
 public class MapRenderSystem extends EngineSystem {
 
     @Override
     public void update(final float deltaTime) {
-        ImmutableArray<Entity> all = getAll(CameraComponent.class);
-        ImmutableArray<Entity> maps = getAll(MapComponent.class);
-        for (final Entity map : maps) {
+        doApply(map -> doApply(camera -> {
             MapComponent mapComponent = Mappers.map.get(map);
-            for (final Entity camera : all) {
-                CameraComponent cameraComponent = Mappers.camera.get(camera);
-                mapComponent.mapRenderer.setView(cameraComponent.orthographicCamera);
-                mapComponent.mapRenderer.render();
-            }
-        }
+            CameraComponent cameraComponent = Mappers.camera.get(camera);
+            mapComponent.mapRenderer.setView(cameraComponent.orthographicCamera);
+            mapComponent.mapRenderer.render();
+        }, CameraComponent.class), MapComponent.class);
     }
 }
